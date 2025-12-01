dead919a6489642af6d5d3ada313d11eb5cc2189
kannar <adamkannar2008@gmail.com>
2025-12-01 14:05:05 +0100

    asd

--- P20251201/UnitTest1.cs
+++ P20251201/UnitTest1.cs
@@ -13,7 +13,30 @@ namespace P20251201
         public void Test1()
         {
             //Assert.Pass();
+            
             StringAssert.Contains("@","Pelda@valami.hu");
+
+        }
+        public void Test2()
+        {
+            //Assert.Pass();
+
+            StringAssert.Contains("@", "Peldavalami.hu");
+
+        }
+        public void Test3()
+        {
+            //Assert.Pass();
+
+            StringAssert.Contains(".", "Pelda@valami.hu");
+
+        }
+        public void Test4()
+        {
+            //Assert.Pass();
+
+            ClassicAssert.Equals(1,2);
+
         }
     }
 }
