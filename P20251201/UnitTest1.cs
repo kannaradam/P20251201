854a71139312f4bec05efcad02a4488c1842f5db
kannar <adamkannar2008@gmail.com>
2025-12-01 14:07:44 +0100

    asad

--- P20251201/UnitTest1.cs
+++ P20251201/UnitTest1.cs
@@ -17,6 +17,7 @@ namespace P20251201
             StringAssert.Contains("@","Pelda@valami.hu");
 
         }
+        [Test]
         public void Test2()
         {
             //Assert.Pass();
@@ -24,6 +25,7 @@ namespace P20251201
             StringAssert.Contains("@", "Peldavalami.hu");
 
         }
+        [Test]
         public void Test3()
         {
             //Assert.Pass();
@@ -31,11 +33,20 @@ namespace P20251201
             StringAssert.Contains(".", "Pelda@valami.hu");
 
         }
+        [Test]
         public void Test4()
         {
             //Assert.Pass();
 
             ClassicAssert.Equals(1,2);
+
+        }
+        [Test]
+        public void Test5()
+        {
+            //Assert.Pass();
+            int jelszohossz = 8;
+            ClassicAssert.AreEqual(jelszohossz, 8);
 
         }
     }
